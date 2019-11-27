a6ed37996e44b042bba3b0a3d06a071f278e6306
NathalieGout <47116074+NathalieGout@users.noreply.github.com>
2019-11-27 12:22:51 +0100

    Add files via upload

---
+++ PlayersView.java
@@ -0,0 +1,51 @@
+package view;
+
+import java.util.Scanner;
+
+public class PlayersView {
+
+	public PlayersView() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int displayNbre() {
+		
+		System.out.println("Combien y t-il de joueurs?");
+		
+		@SuppressWarnings("resource")
+		Scanner sc=new Scanner(System.in);
+		int str= sc.nextInt();
+		return str;
+		
+		
+		
+		
+	}
+	
+	public  String displayNom(){
+System.out.println("Quel est ton prénom?");
+		
+		@SuppressWarnings("resource")
+		Scanner sc=new Scanner(System.in);
+		String str= sc.nextLine();
+		return str;
+		
+		
+		
+	}
+	public char symbol() {
+		System.out.println("Choisi ton symbol");
+		
+		Scanner sc=new Scanner(System.in);
+		String str= sc.nextLine();
+		char lValue = str.charAt(0);
+		sc.close();
+		return lValue;
+		
+		
+		
+		
+		
+	}
+
+}
